dce6f3d86a7d21a4853bfeae157ca07c3ac0aa68
Artyom <as.fedotkin@yandex.ru>
2018-01-12 16:46:43 +0300

    work in click method

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  //this function shuffles the elements in the array
   function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -17,27 +18,23 @@ $(document).ready(function() {
   }
 
   return array;
-}
+  }// end function shuffle ();
 
 
   var arrayWithNameElements = ['#element1','#element2','#element3','#element4','#element5','#element6','#element7','#element8','#element9','#element10','#element11','#element12','#element13','#element14','#element15','#element16'];
   var arrayWithValue = ['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8'];
-  //arrayWithValue = arrayWithValue.sort;
 
+  //shuffle array
   arrayWithValue = shuffle(arrayWithValue);
 
-
-  jQuery('#console').html(arrayWithValue[5]);
-  //jQuery(arrayWithNameElements[4]).html (arrayWithValue[5]);
+  //назначаем каждому элементу на странице перетасованный элемент массива
   for (var i=0; i<16; i++) {
-    jQuery(arrayWithNameElements[i]).html (arrayWithValue[i])
+    jQuery(arrayWithNameElements[i]).html (arrayWithValue[i]);
   }
 
+  
 
 
-//  for (i=0; i<arrayWithValue.length; i++) {
-    //$(arrayWithNameElements[i]).val(arrayWithValue[i]);
-//  }
 
 //var str = 'Привет';
 //jQuery('#console').html(str);
